82c4d404ac4f8711329ac8d5e7dfca17680326d1
Naman Sharma <sharma.namanvats811@gmail.com>
2024-10-26 22:51:20 +0530

    Successfully added my projects pdf and project video

---
+++ medium level project/script.js
@@ -0,0 +1,22 @@
+function clearDisplay() {
+    document.getElementById('result').value = '';
+}
+
+function deleteLast() {
+    const display = document.getElementById('result');
+    display.value = display.value.slice(0, -1);
+}
+
+function appendToDisplay(value) {
+    const display = document.getElementById('result');
+    display.value += value;
+}
+
+function calculateResult() {
+    const display = document.getElementById('result');
+    try {
+        display.value = eval(display.value.replace('×', '*').replace('÷', '/'));
+    } catch (e) {
+        display.value = 'Error';
+    }
+}
